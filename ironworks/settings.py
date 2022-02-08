88ff19775d35c10219b826e850df923ccf38ccf0
Charlie Watson <charliewatson87@gmail.com>
2022-02-08 18:03:34 +0000

    feat: create a base.html and link to index.html

--- ironworks/settings.py
+++ ironworks/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["booking-system-ironworks.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["booking-system-ironworks.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
