abf6a8aa3a45b8b1ea82bd003e1adc8442283acb
Charlie Watson <charliewatson87@gmail.com>
2022-02-24 09:41:07 +0000

    feat: add choices to pt_bookings models.py

--- pt_bookings/models.py
+++ pt_bookings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+time_choices = (
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("12:00", "12:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+    ("17:00", "17:00"),
+    )
+
+status_choices = (
+    ("confirmed","confirmed"),
+    ("rejected","rejected"),
+    ("pending","pending"),
+)
