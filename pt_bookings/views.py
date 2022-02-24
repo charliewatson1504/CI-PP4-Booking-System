a10e2b40201b79a958af08f202c3f07255a755e8
Charlie Watson <charliewatson87@gmail.com>
2022-02-24 21:44:49 +0000

    feat: create book a session view

--- pt_bookings/views.py
+++ pt_bookings/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+import datetime
+
+from .models import BookPTSession
+from .forms import BookingForm
+
 
 # Create your views here.
+class BookASession(View):
+    """
+    This view allows users to book a session
+    """
+    def get(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            booking_form = BookingForm()
+        else:
+            messages.add_message(
+                request, messages.ERROR,
+                "Sorry, you are not logged in."
+                "Please login here.")
+            url = reverse('login')
+            return HttpResponseRedirect(url)
+        
+        return render(request, "bookings.html",
+                        {'booking_form': booking_form})
+
+
