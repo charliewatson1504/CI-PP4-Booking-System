df831f1a2948ab38717b9797fd0ac74f59070dfd
Charlie Watson <charliewatson87@gmail.com>
2022-02-24 23:19:33 +0000

    feat: add book button and header to booking form

--- pt_bookings/forms.py
+++ pt_bookings/forms.py
@@ -1,6 +1,7 @@
 import imp
 from pyexpat import model
 from django import forms
+from django.conf import settings
 
 from .models import BookPTSession
 
@@ -9,6 +10,9 @@ class BookingForm(forms.ModelForm):
     """
     A class for the PT booking form
     """
+
+    requested_date = forms.DateField(input_formats=settings.DATE_INPUT_FORMAT)
+
     class Meta:
         model = BookPTSession
         fields = ('staff', 'requested_date', 'requested_time')
